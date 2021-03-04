3bbaae41d96e866be06d0e3c70f14fb2d4ad7629
kane-young <lyw2100@naver.com>
2021-03-04 14:50:34 +0900

    update something

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,8 +1,24 @@
+//
+//  RockPaperScissors - main.swift
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
 class RockPaperScissorsGame {
-    enum Hand: Int, CaseIterable {
-        case scissors = 1
-        case rock = 2
-        case paper = 3
+    enum Hand: String, CaseIterable, Comparable {
+        static func < (lhs: RockPaperScissorsGame.Hand, rhs: RockPaperScissorsGame.Hand) -> Bool {
+            if (lhs == .scissors && rhs == .rock)
+            || (lhs == .paper && rhs == .scissors)
+            || (lhs == .rock && rhs == .paper) {
+                return true
+            } else {
+                return false
+            }
+        }
+        
+        case scissors = "1"
+        case rock = "2"
+        case paper = "3"
     }
     
     enum GameError: Error {
@@ -16,12 +32,14 @@ class RockPaperScissorsGame {
     }
 
     func startGame() {
-        while true {
-            guard let computersHand = makeRandomHand() else {
-                return
+        var isRepeat = false
+        
+        repeat {
+            guard let computersHand = Hand.allCases.randomElement() else {
+                continue
             }
             
-            var userHand: Hand = .paper
+            var userHand: Hand
             do {
                 if let notNilUserHand = try getHandByUser() {
                     userHand = notNilUserHand
@@ -31,56 +49,43 @@ class RockPaperScissorsGame {
                 }
             } catch {
                 print("잘못된 입력입니다. 다시 입력해주세요")
+                isRepeat = true
                 continue
             }
-
-            let gameResult = getGameResult(userHand, vs: computersHand)
-            showResult(gameResult)
-        }
+            
+            isRepeat = gameResult(userHand, vs: computersHand)
+        } while isRepeat
     }
     
-    func makeRandomHand() -> Hand? {
-        let hands = Hand.allCases
-        let random = hands.randomElement()
-        return random
-    }
-    
-    func showResult(_ input: GameResult) { // startGame 함수에서 showResult 분리하기
-        let resultStatement = input.rawValue
-        print(resultStatement)
-    }
-    
-    func getGameResult(_ usersHand: Hand, vs computersHand: Hand) -> GameResult {
+    func gameResult(_ usersHand: Hand, vs computersHand: Hand) -> Bool {
         if usersHand == computersHand {
-            return .draw
-        } else if (usersHand == .rock && computersHand == .scissors)
-            || (usersHand == .scissors && computersHand == .paper)
-            || (usersHand == .paper && computersHand == .rock) {
-            return .win
+            print("비겼습니다.")
+            return true
+        } else if usersHand > computersHand {
+            print("이겼습니다.")
+            return false
         } else {
-            return .lose
+            print("졌습니다.")
+            return false
         }
     }
         
     func getHandByUser() throws -> Hand? {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
         
-        guard let stringUserInput = readLine(), let integerUserInput = Int(stringUserInput) else {
+        guard let userInput = readLine() else {
             throw GameError.invalidInput
         }
         
-        switch integerUserInput {
-        case 0:
+        if userInput == "0" {
             return nil
-        case Hand.scissors.rawValue:
-            return Hand.scissors
-        case Hand.rock.rawValue:
-            return Hand.rock
-        case Hand.paper.rawValue:
-            return Hand.paper
-        default:
+        }
+        
+        guard let userHand = Hand(rawValue: userInput) else {
             throw GameError.invalidInput
         }
+
+        return userHand
     }
 }
 
